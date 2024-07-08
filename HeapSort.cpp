43955a58099f22c7ae77ce57ad826a312d18eb52
SUJAN GANIGA <sujan.ganiga2020@gmail.com>
2024-07-08 23:48:40 +0530

    08-07-2024

--- HeapSort.cpp
+++ HeapSort.cpp
@@ -9,9 +9,9 @@ void heapify(int arr[], int n, int i)
     int largest = i;       // Initialize largest as root
     int left = 2 * i + 1;  // Left child
     int right = 2 * i + 2; // Right child
-    if (left < n && arr[left] < arr[largest])
+    if (left < n && arr[left] > arr[largest])
         largest = left; // If left child is larger than root
-    if (right < n && arr[right] < arr[largest])
+    if (right < n && arr[right] > arr[largest])
         largest = right; // If right child is larger than largest so far
 
     if (largest != i)
